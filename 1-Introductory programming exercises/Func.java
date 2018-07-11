a055fcb36c93dc593ef289b42ac6b6fc0f943b52
LIU-Hanwen <liu.hanwen@foxmail.com>
2018-07-11 16:50:02 +0800

    finish the second one

--- 1-Introductory programming exercises/Func.java
+++ 1-Introductory programming exercises/Func.java
@@ -3,4 +3,13 @@ public class Func{
         String ret = "*";
         return ret;
     };
+
+    public static String draw_a_horizontal_line(int n){
+        
+        String ret = "";
+        for(int i = 0; i<n; i+=1){
+            ret = ret.concat(Func.print_one_asterisk());
+        }
+        return ret;
+    };
 }
--- 1-Introductory programming exercises/Main.java
+++ 1-Introductory programming exercises/Main.java
@@ -2,7 +2,9 @@ import Func;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("# Easiest exercise ever\nPrint one asterisk to the console.\nOutput:");
+        System.out.println("1. Output:");
         System.out.println(Func.print_one_asterisk());
+        System.out.println("2. Output:");
+        System.out.println(Func.draw_a_horizontal_line(8));
     }
 }
