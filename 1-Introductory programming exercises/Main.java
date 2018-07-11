0b086e4821ea30385ed7f4e0e82c67d125b71be9
LIU-Hanwen <liu.hanwen@foxmail.com>
2018-07-11 16:51:59 +0800

    finish the third one

--- 1-Introductory programming exercises/Main.java
+++ 1-Introductory programming exercises/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println(Func.print_one_asterisk());
         System.out.println("2. Output:");
         System.out.println(Func.draw_a_horizontal_line(8));
+        System.out.println("3. Output:");
+        System.out.println(Func.draw_a_vertical_line(3));
     }
 }
